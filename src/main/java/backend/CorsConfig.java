fce45a08cb92819e8d79630917f205ad0e57d9c2
SaatvikaR <saatvika0507@gmail.com>
2026-03-23 18:11:28 +0530

    link

--- src/main/java/backend/CorsConfig.java
+++ src/main/java/backend/CorsConfig.java
@@ -6,14 +6,16 @@ import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
+                        .allowedOrigins("https://eduseat-frontend.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
--- src/main/java/backend/SeatingController.java
+++ src/main/java/backend/SeatingController.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://eduseat-frontend.vercel.app/")
 public class SeatingController {
 
     @Autowired
